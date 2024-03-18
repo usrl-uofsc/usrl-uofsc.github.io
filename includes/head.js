60bf20587fb00dde17ab4377d8efcc30ab0c71fe
Nikolaos Sarantinoudis <nsarantinoudis@tuc.gr>
2024-03-18 18:00:33 -0400

    Fixing icons in people

--- includes/head.js
+++ includes/head.js
@@ -13,7 +13,7 @@ document.write(`
   <!-- Material Design Bootstrap -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.2/css/mdb.min.css" rel="stylesheet">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- Custom styles for this template -->
   <link href="style/main.css" rel="stylesheet">
   <link href="style/publications.css" rel="stylesheet">
