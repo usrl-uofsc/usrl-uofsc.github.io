0428c4e352abe09a00f0055fc969e6825885abb6
Nikolaos Sarantinoudis <nsarantinoudis@tuc.gr>
2024-04-03 18:09:17 -0400

    Fixing Links

--- includes/nav.js
+++ includes/nav.js
@@ -47,7 +47,7 @@ document.write(`
 $(function(){
 
     var url = window.location.pathname, 
-        urlRegExp = new RegExp(url == '/' ? window.location.origin + '/?$' : url.replace(/\/$/,'')); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
+        urlRegExp = new RegExp(url.replace(/\/$/,'') + "$"); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
         // now grab every link from the navigation
         $('#navbarContent a').each(function(){
             // and test its normalized href against the url pathname regexp
