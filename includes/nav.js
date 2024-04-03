1b53eef630b8c5a3f45052c82a42a9d1ae36b524
Nikolaos Sarantinoudis <nsarantinoudis@tuc.gr>
2024-04-03 17:14:36 -0400

    Publication Links

--- includes/nav.js
+++ includes/nav.js
@@ -48,6 +48,7 @@ $(function(){
 
     var url = window.location.pathname, 
         urlRegExp = new RegExp(url == '/' ? window.location.origin + '/?$' : url.replace(/\/$/,'')); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
+        $('#navbarContent a').removeClass('active');
         // now grab every link from the navigation
         $('#navbarContent a').each(function(){
             // and test its normalized href against the url pathname regexp
