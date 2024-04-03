2cd5d833cfb922f90100c7b63b71321673f518ac
Nikolaos Sarantinoudis <nsarantinoudis@tuc.gr>
2024-04-03 16:47:41 -0400

    Fixing Menu

--- includes/nav.js
+++ includes/nav.js
@@ -13,8 +13,8 @@ document.write(`
   <nav class="navbar navbar-expand-lg navbar-light p-0 navbar-bottom">
     <div class="collapse navbar-collapse bg-light px-3" id="navbarContent">
       <ul class="navbar-nav mx-md-auto mx-0 w-50 align-items-start nav-fill">
-        <li class="nav-item">
-          <a class="nav-link active" href="index.html">Home</a>
+        <li class="nav-item" >
+          <a class="nav-link" href="index.html">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="research.html">Research</a>
@@ -43,3 +43,17 @@ document.write(`
 
   </nav>
 `)
+
+$(function(){
+
+    var url = window.location.pathname, 
+        urlRegExp = new RegExp(url == '/' ? window.location.origin + '/?$' : url.replace(/\/$/,'')); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
+        // now grab every link from the navigation
+        $('#navbarContent a').each(function(){
+            // and test its normalized href against the url pathname regexp
+            if(urlRegExp.test(this.href.replace(/\/$/,''))){
+                $(this).addClass('active');
+            }
+        });
+
+});
